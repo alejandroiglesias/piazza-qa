8ebb9e7b0fce94a05a91621a0739a04aaf7efdac
Alejandro García Iglesias <ale.garciaiglesias@gmail.com>
2020-01-20 01:20:12 -0300

    Make posts data have same members for type compatibility

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -8,16 +8,20 @@ export const posts = [
         hasInstructorsAnswer: true,
         hasStudentsAnswer: true,
         id: 1,
+        isRead: undefined,
         title: 'anybody know when grades go on bearfacts?',
         replyCount: 2,
       },
       {
-        id: 2,
         date: '5/14/12',
         details: `The course letter grade cutoffs are (the percentages are rounded and may not add up to exactly 100):
-
         A+	96-100	3.57% of`,
+        hasInstructorsAnswer: undefined,
+        hasStudentsAnswer: undefined,
+        id: 2,
+        isRead: undefined,
         title: 'Course letter grade cutoffs and statistics',
+        replyCount: undefined,
       },
     ],
   },
@@ -25,23 +29,25 @@ export const posts = [
     week: 'Week 5/6 - 5/12',
     posts: [
       {
-        id: 3,
         date: '5/8/12',
         details: `I have no idea what to do with my answer from 8(a)
           Help!!!!
 
            #final #practice.final #pin`,
         hasInstructorsAnswer: true,
+        hasStudentsAnswer: undefined,
+        id: 3,
+        isRead: undefined,
         replyCount: 1,
         title: '8(b) on practice final',
       },
       {
-        id: 4,
         date: '5/6/12',
         details:
           "i tried series and still can't get an exact number...wolfram alpha tells me it should be 0.48 something but i don't unde",
         hasInstructorsAnswer: true,
         hasStudentsAnswer: true,
+        id: 4,
         isRead: true,
         replyCount: 3,
         title: 'how do you integrate arctanx/x on [0,1/2]',
@@ -52,19 +58,22 @@ export const posts = [
     week: 'Week 4/29 - 5/5',
     posts: [
       {
-        id: 5,
         date: '5/5/12',
         details: `I know the final is inclusive of everything we're learned this semester, but how likely will Euler's method (using the s`,
         hasInstructorsAnswer: true,
+        hasStudentsAnswer: undefined,
+        id: 5,
+        isRead: undefined,
         replyCount: 1,
         title: "Euler's method (step size)",
       },
       {
-        id: 6,
         date: '5/6/12',
         details:
           "Can someone point me in a direction to start? I'm pretty brain dead at this point and have been going in circles for a w",
         hasInstructorsAnswer: true,
+        hasStudentsAnswer: undefined,
+        id: 6,
         isRead: true,
         replyCount: 1,
         title: 'Problem 4b on the practice final',
